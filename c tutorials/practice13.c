68d8179af708b2d974f0fc5ddc3c93ede9f1eb07
ranjansharma27 <ranjankumarsharma27@gmail.com>
2024-09-24 22:41:55 +0530

    Ranjan

---
+++ c tutorials/practice13.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    // increment operator
+    // ++i (pre increment)
+    // i++(post increment)
+    for (int i = 0; i <= 10; i =i+1){
+        printf("%d\n", i);
+        
+    }
+    return 0;
+    
+}
---
+++ c tutorials/testFOR.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    //iterator: counter
+    for (int i=1; i<=5; i = i+1){
+        printf("hellod world\n");    
+            
+        }
+        return 0;
+    
+}
+
