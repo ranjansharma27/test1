72b4a39bc7a30972f5e1ddcfbed33c92d5f3006a
ranjansharma27 <ranjankumarsharma27@gmail.com>
2024-09-26 23:05:14 +0530

    ranjan

---
+++ c tutorials/FunctionTEST.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+//declaration/prototype
+void printhello();
+
+
+int main() {
+    printhello();// function call
+    printhello();
+    printhello();
+    printhello();
+    printhello();
+    printhello();
+    return 0;
+}
+
+
+//function definition
+void printhello() {
+    printf("hello!\n ");
+
+}
