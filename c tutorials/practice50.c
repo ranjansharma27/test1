c20946ad438b1722ebcabe20bb1df1c4942e72f1
ranjansharma27 <ranjankumarsharma27@gmail.com>
2024-10-10 11:05:45 +0530

    ranjan

---
+++ c tutorials/practice50.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+void printString(char arr[]);
+int countLength(char arr[]);
+
+int main(){
+    char str[100];
+    char ch;
+    int i =0;
+
+    while (ch !='\n'){
+        scanf("%c",&ch);
+        str[i] =ch;
+        i++;
+        
+    }
+    str[i]='\0';
+    puts(str);
+    
+}
