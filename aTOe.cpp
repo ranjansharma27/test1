b0c87d517d27e7d798b75851e243cf4eed7563e0
ranjansharma27 <ranjankumarsharma27@gmail.com>
2024-10-17 12:33:23 +0530

    ranjan c++

---
+++ aTOe.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int row,col;
+
+    for(row=1;row<=5;row=row+1){
+        for(char col='a'; col<='e'; col=col+1)
+        {
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+}
