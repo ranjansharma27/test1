8b3c1f0874fb20c6296db09f24532fc658735ad2
ranjansharma27 <ranjankumarsharma27@gmail.com>
2024-10-19 16:59:27 +0530

    ranjan c++

---
+++ C++ Language/bitwish.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int ans=2^3;
+    cout<<"My answer is: "<<ans<<endl;
+}
---
+++ C++ Language/comparison.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter the number: ";
+    cin>>a;
+    cout<<"Enter the number: ";
+    cin>>b;
+
+
+    if(a==b)
+    cout<<"yes";
+    else
+    cout<<"no";
+}
