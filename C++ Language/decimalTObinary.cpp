394c48a86832c92a27b1f1c3499505159c74dd3e
ranjansharma27 <ranjankumarsharma27@gmail.com>
2024-10-21 23:04:16 +0530

    ranjan c++

---
+++ C++ Language/decimalTObinary.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter the number: ";
+    cin>>num;
+    int rem,ans=0,mul=1;
+    while (num>0)
+    {
+        rem= num%2;
+
+        num=num/2;
+
+        ans = rem*mul+ans;
+
+        mul=mul*10;
+
+    }
+    cout<<ans<<endl;
+}
