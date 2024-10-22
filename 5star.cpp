06e7522dcc308f9c2ac70f9f52bccb7805e7ef23
ranjansharma27 <ranjankumarsharma27@gmail.com>
2024-10-22 11:00:37 +0530

    ranjan c++

--- 5star.cpp
+++ 5star.cpp
@@ -5,7 +5,7 @@ int main(){
     int row,col;
     for(row=1;row<=5;row=row+1){
         for(col=1;col<=5-(row-1);col=col+1){
-            cout<<"*"<<" ";
+            cout<<"**"<<" ";
 
         }
         cout<<endl;
